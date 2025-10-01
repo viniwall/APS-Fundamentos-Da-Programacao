ff507332786020c9a5f573c967c7e119d7f2d72a
vinicius <vinicius.waldrigues@unifebe.edu.br>
2025-10-01 15:11:16 -0300

    Added obesidade and Media

---
+++ APS vinicius/Media/Media.java
@@ -0,0 +1,18 @@
+package Media;
+import java.util.Scanner;
+
+public class Media {
+    public static void Main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("INSIRA AS TRÊS NOTAS:");
+        double Nota1 = s.nextDouble();
+        double Nota2 = s.nextDouble();
+        double Nota3 = s.nextDouble();
+
+        double media = (Nota1+Nota2+Nota3)/3;
+
+        System.out.println("SUA MÉDIA É: " + media);
+    s.close();
+    }
+}
--- APS vinicius/obesidade/obesidade.java
+++ APS vinicius/obesidade/obesidade.java
@@ -6,14 +6,34 @@ public class obesidade {
         boolean repetir = true;
         Scanner s = new Scanner(System.in);
 
+        while (repetir) {
         System.out.println("INSIRA O PESO");
         double peso = s.nextDouble();
+        s.nextLine();
         System.out.println("INSIRA A ALTURA");
         double altura = s.nextDouble();
+        s.nextLine();
         double imc = peso/(altura*altura);
         
-        for (double imc = 18.5;imc <= 18.5;imc --) {
-
+        if (imc<18.5) {
+            System.out.println("VOCÊ ESTA ABAIXO DO PESO IDEAL!");
+        } else if (imc>18.6&&imc<24.9) {
+            System.out.println("VOCÊ ESTA DENTRO DO PESO IDEAL. PARABÉNS!");
+        } else if (imc>25&&imc<29.9) {
+            System.out.println("VOCÊ ESTA LEVEMENTE ACIMA DO PESO!");
+        } else if (30<imc&&imc>34.9) {
+            System.out.println("VOCÊ TEM OBESIDADE GRAU 1");
+        } else if (35<imc&&imc>39.9) {
+            System.out.println("VOCÊ TEM OBESIDADE GRAU 2 (SEVERA)");
+        } else if (imc>40) {
+            System.out.println("VOCÊ TEM OBESIDADE GRAU 4 (MORBIDA). PROCURE AJUDA!");
         }
+        System.out.println("REPETIR CÁLCULO?");
+        String resposta = s.nextLine();
+        if (!resposta.equalsIgnoreCase("s")) {
+            repetir = false;
+        }
+    }
+    s.close();
     } 
 }
