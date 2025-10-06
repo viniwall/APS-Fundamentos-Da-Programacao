26199bdc7eb1346b45b3d0cbfecdb93aa8ea8110
vinicius <vinicius.waldrigues@unifebe.edu.br>
2025-10-05 21:47:23 -0300

    Iniciado GasolinaLitros

---
+++ APS vinicius/GasolinaLitros/GasolinaLitros.java
@@ -0,0 +1,12 @@
+package GasolinaLitros;
+import java.util.Scanner;
+
+public class GasolinaLitros {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("QUANTOS KMs SERÃO RODADOS:");
+        double Kms = s.nextDouble();
+        
+    }
+}
